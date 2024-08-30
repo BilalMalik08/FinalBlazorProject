f2ab101e11c0738372dfe84af93f2d41da6aec82
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-08-31 01:09:37 +0500

    Updated 1:13 AM, 31 August, 2024

---
+++ YumBlazor/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please rnter name.")]
+        public string Name { get; set; }
+    }
+}
