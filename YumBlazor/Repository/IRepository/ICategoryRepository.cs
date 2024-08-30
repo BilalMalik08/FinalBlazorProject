dcace5c43e276820d5cf1cd3e58cf7661290d933
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-08-31 01:14:53 +0500

    Updated 1:19 AM, 31 August, 2024

---
+++ YumBlazor/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Category Create(Category obj);
+        public Category Update(Category obj);
+        public bool Delet(int id);
+        public Category Get(int id);
+        public IEnumerable<Category> GetAll();
+
+    }
+}
