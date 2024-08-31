68d18ceeb64f5da0aa08c4636c07ea8c4fd67c60
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-09-01 01:40:16 +0500

    Updated 1:44 AM, 1 September, 2024

---
+++ YumBlazor/Data/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YumBlazor.Data
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(500, 5000)]
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? SpecialTag { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
