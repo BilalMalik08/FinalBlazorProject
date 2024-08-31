8302e9286510c5320f2c3f1bc2bc09896855061d
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-08-31 22:17:31 +0500

    Updated 10:21 PM, 31 August, 2024

---
+++ YumBlazor/Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
