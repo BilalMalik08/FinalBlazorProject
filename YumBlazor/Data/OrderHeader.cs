283dfc7f9759d87accaaec773aebbcee803fb028
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-09-04 19:29:33 +0500

    Updated 7:34 PM, 4 September, 2024

---
+++ YumBlazor/Data/OrderHeader.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        [Display(Name = "Order Total")]
+        public double OrderTotal { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+    }
+}
