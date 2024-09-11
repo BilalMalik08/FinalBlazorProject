6f196471e7abe7ddc5fb7ac665cd2fa525f9c8ad
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-09-11 18:24:38 +0500

    Updated 6:30 PM, 11 September, 2024

---
+++ YumBlazor/Services/SharedStateService.cs
@@ -0,0 +1,20 @@
+﻿namespace YumBlazor.Services
+{
+    public class SharedStateService
+    {
+        public event Action OnChange;
+        public int _totalCartCount;
+
+        public int TotalCartCount 
+        { 
+            get => _totalCartCount; 
+            set 
+            { 
+                _totalCartCount = value; 
+                NotifyStateChanged(); 
+            } 
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
